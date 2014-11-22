0243be0c4d113452c4d93cbf2f6ad35a4f829f89
kannan <kannan.90@gmail.com>
2014-11-22 19:00:19 +0800

    add account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
+  before_create :create_activation_digest
   before_save { self.email.downcase! }
   validates :name, presence: true, length: { maximum: 50 } 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -8,6 +9,7 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }, allow_blank: true
   has_secure_password
+
 
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
@@ -24,12 +26,24 @@ class User < ActiveRecord::Base
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  def authenticated?(attribute, token)
+    digest = self.send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
   end
 
   def forget
     update_attribute(:remember_digest, nil)
   end
+
+  def activate
+    update_attribute(:activated, true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  private
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
+
   test "invalid signup info" do
     get signup_path
     assert_no_difference "User.count" do
@@ -12,14 +17,31 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template 'users/new'
   end
 
-  test "valid signup info" do
+  test "valid signup info with account activation" do
     get signup_path
     assert_difference "User.count", 1 do
-      post_via_redirect users_path, user: { name: "Example User",
+      post users_path, user: { name: "Example User",
                                email: "user@invalid.com",
                                password: "foobar",
                                password_confirmation: "foobar" }
     end
+
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    user = assigns(:user)
+    assert_not user.activated?
+    #try before activation
+    log_in_as user
+    assert_not is_logged_in?
+    #invalid activation token
+    get edit_account_activation_path("invalidness")
+    assert_not is_logged_in?
+    #invalid email
+    get edit_account_activation_path(user.activation_token, email:"invalidness")
+    assert_not is_logged_in?
+    #valid everything!
+    get edit_account_activation_path(user.activation_token, email:user.email)
+    assert user.reload.activated?
+    follow_redirect!
     assert_template 'users/show'
     assert is_logged_in?
   end
