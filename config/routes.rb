91a5ecf77b6a7a97a0e630b29b2c908d0840fbb2
kannan <kannan.90@gmail.com>
2014-11-22 20:38:46 +0800

    add password resets

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+
   get 'sessions/new'
 
   get 'users/new'
@@ -16,6 +18,7 @@ Rails.application.routes.draw do
 
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 
 
   # The priority is based upon order of creation: first created -> highest priority.
